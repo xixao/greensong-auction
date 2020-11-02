cd4cdf94cce95180d8f90dd8e0f45993c5586017
Steve Melcher <steven_melcher@homedepot.com>
2020-11-02 14:13:13 -0500

    Simplified email regex

--- src/components/BidModal/index.js
+++ src/components/BidModal/index.js
@@ -44,7 +44,7 @@ export default class Menu extends Component {
 
       const unformattedPhone = bidderPhone.replace(/\D/g, '')
 
-      const emailRegex = /(?:[a-z0-9!#$%&'*+\=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+      const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/;
 
       if (!bidderName) {
         alert('Please enter your name')
