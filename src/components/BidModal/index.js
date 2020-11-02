119dec7e344091abe7e1746942d47e5b1434c714
Steve Melcher <steven_melcher@homedepot.com>
2020-11-02 14:00:49 -0500

    Fixed regex pattern

--- src/components/BidModal/index.js
+++ src/components/BidModal/index.js
@@ -44,8 +44,7 @@ export default class Menu extends Component {
 
       const unformattedPhone = bidderPhone.replace(/\D/g, '')
 
-      /*eslint no-useless-escape: "error"*/
-      const emailRegex = /^(([^<>()\[\]\.,;:\s@"]+(\.[^<>()\[\]\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const emailRegex = /^(([^<>()[]\.,;:\s@"]+(\.[^<>()[].,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
       if (!bidderName) {
         alert('Please enter your name')
